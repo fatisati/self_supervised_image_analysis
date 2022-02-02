e8f769295840db915abcfddcfa3e09db9e78a77c
fatisati@gmail.com <fatisati@gmail.com>
2022-02-02 14:08:04 +0330

    barlow-twins call method implemented

--- barlow/barlow_pretrain.py
+++ barlow/barlow_pretrain.py
@@ -49,6 +49,9 @@ class BarlowTwins(tf.keras.Model):
         self.lambd = lambd
         self.loss_tracker = tf.keras.metrics.Mean(name="loss")
 
+    def call(self, x):
+        return self.encoder(x)
+
     @property
     def metrics(self):
         return [self.loss_tracker]

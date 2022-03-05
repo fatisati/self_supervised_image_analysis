7a1bcbe2fe24af1e2f0de8973a091ce7d0f7654b
fatisati@gmail.com <fatisati@gmail.com>
2022-03-05 18:00:29 +0330

    debug supervised_ds

--- datasets/supervised_ds.py
+++ datasets/supervised_ds.py
@@ -10,10 +10,16 @@ class SupervisedDs:
         self.image_urls = [image_folder + name for name in image_names]
         self.labels = labels
 
-    def get_ds(self, aug_func, batch_size):
-        img_ds = tf.data.Dataset.from_tensor_slices(self.image_urls).map(lambda img: read_tf_image(img, 512))
-        img_ds = img_ds.map(aug_func)
-        labels_ds = tf.data.Dataset.from_tensor_slices(self.labels)
-        zip_ds = tf.data.Dataset.zip(img_ds, labels_ds)
+    def process_sample(self, url, label, aug_func):
+        img = read_tf_image(url, 512)
+        img = aug_func(img)
+        return img, label
 
-        return zip_ds.batch(batch_size)
+    def get_ds(self, aug_func, batch_size):
+        # .map(lambda img: read_tf_image(img, 512))
+        img_ds = tf.data.Dataset.from_tensor_slices(self.image_urls)
+        # img_ds = img_ds.map(aug_func)
+        labels_ds = tf.data.Dataset.from_tensor_slices(self.labels)
+        ds = tf.data.Dataset.zip((img_ds, labels_ds)).map(lambda img, label: self.process_sample(img, label, aug_func))
+
+        return ds.batch(batch_size)

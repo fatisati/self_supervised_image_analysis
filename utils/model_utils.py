0ccf23e19b71610af21f3d24b78679184ccecc38
fatisati@gmail.com <fatisati@gmail.com>
2022-03-08 11:37:16 +0330

    uing py_function

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -144,7 +144,7 @@ from utils.multi_class_metric import weighted_recall
 
 
 def get_metrics():
-    tf.config.run_functions_eagerly(True)
+    # tf.config.run_functions_eagerly(True)
     METRICS = [
         # keras.metrics.TruePositives(name='tp'),
         # keras.metrics.FalsePositives(name='fp'),
@@ -156,7 +156,7 @@ def get_metrics():
         tf.keras.metrics.Recall(name='recall'),
         tf.keras.metrics.AUC(name='auc'),
         tf.keras.metrics.AUC(name='prc', curve='PR'),  # precision-recall curve
-        lambda y, y_pred: tf.py_function(weighted_recall(y, y_pred))
+        lambda y, y_pred: tf.py_function(weighted_recall, [y, y_pred])
     ]
     return METRICS
 
